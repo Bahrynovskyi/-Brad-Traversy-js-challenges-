43587b96115e66cc9bbc1d26d995e9d60e968d19
Bahrynovskyi <pbahrynovskyi@gmail.com>
2025-03-21 13:50:46 +0200

    Lesson 3

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,27 @@
-function generateHashtag() {}
+// function generateHashtag(str) {
+//     if (str.trim() === '') {
+//         return false;
+//     }
+
+//     const words = str.trim().split(' ');
+
+//     const capitalizedWords = words.map((word) => {
+//         let firstLetter = word.slice(0, 1);
+//         let otherWord = word.slice(1, word.length);
+//         return `${firstLetter.toUpperCase()}${otherWord}`;
+//     });
+//     const hashtag = `#${capitalizedWords.join('')}`;
+//     return hashtag > 140 ? false : hashtag;
+// }
+
+
+function generateHashtag(str) {
+    const hashtag = str.split(' ').reduce(function (tag, word) {
+        return tag + word.charAt(0).toUpperCase() + word.substring(1);
+    }, '#');
+
+    return hashtag.length === 1 || hashtag.length > 140 ? false : hashtag;
+}
 
 module.exports = generateHashtag;
+
